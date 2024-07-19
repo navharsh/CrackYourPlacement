db8f6bc348b5beb8fc8a761f3a772cb0beed7f46
Navharsh <navharshkumar12@gmail.com>
2024-07-19 22:21:09 +0530

    Day 4 Solutions

---
+++ Day4/MoveZeros.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/move-zeroes/submissions/1326275868
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
